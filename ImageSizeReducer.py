205d2849d224181d813cdc5e12738475e79700ca
Beshr Eldebuch <bshr505@gmail.com>
2020-02-17 12:00:08 +0200

    Add Unicode support for path file names [Replacing cv2.imread() and cv2.imwrite() to np.fromfile() and np,tofile() ]

--- ImageSizeReducer.py
+++ ImageSizeReducer.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import numpy as np
 
 class ImageSizeReducer:
   def __init__(self, width=800, height=500, jpg_quality=30):
@@ -7,13 +8,14 @@ class ImageSizeReducer:
     self.input_path = ""
     self.dim = (width, height)
     self.jpg_quality = jpg_quality
+    self.num_compressed_image = 0
 
   def check_path(self, path):
     if os.path.isdir(path):
       return path
     else:
       raise UserWarning("Enter a valid directory path ... {}   isn't valid".format(path))
-  
+
   def __set_output_path(self, input_path):
 
     self.output_path =  os.path.join(input_path, "Output")
@@ -32,6 +34,17 @@ class ImageSizeReducer:
     #jpg_quality {int} -- 0 - 100 (higher means better). (default: {30})
     self.jpg_quality = jpg_quality
 
+  def read_image_unicode(self, path):
+    return cv2.imdecode(np.fromfile(path, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
+
+  def write_image_unicode(self, path, img):
+    """Since OpenCV Support only Ascii, To write Unicode Image
+    we can first encode the image in OpenCV format to one dimension numpy ndarray format.
+    Then we convert this numpy ndarray to image on disk with the tofile() method.
+    """
+    _ , im_buf_arr = cv2.imencode(".jpg", img, [int(cv2.IMWRITE_JPEG_QUALITY), self.jpg_quality])
+    im_buf_arr.tofile(path)
+
   def save(self, path, image):
     """resize and save given image
     
@@ -42,14 +55,18 @@ class ImageSizeReducer:
     Keyword Arguments:
         jpg_quality {int} -- 0 - 100 (higher means better). (default: {50})
     """
+    print("test  ", path)
     resized = cv2.resize(image, self.dim, interpolation = cv2.INTER_AREA)
-    cv2.imwrite(path, resized, [int(cv2.IMWRITE_JPEG_QUALITY), self.jpg_quality])
+    self.write_image_unicode(path, resized)
     print("{} is added successfully \n ".format( path))
+
+  def get_num_compressed_image(self):
+    return self.num_compressed_image
 
   def folder_reader(self):
     """read all (JPG,JPEG,PNG) pictures exists in input_path
     """
-    self.sucsess = False
+    self.num_compressed_image = 0 
     for f in os.listdir(self.input_path):
       if f.endswith((".JPG", ".jpeg", ".png", ".jpg")):
           file_path = os.path.join(self.input_path, f)
@@ -57,12 +74,15 @@ class ImageSizeReducer:
           name = self.name_without_ext(f)
           ext = ".JPG"
           full_name = name + ext
-          self.save(path= os.path.join(self.output_path, full_name ), image= cv2.imread(file_path))
-          self.sucsess = True
-    
-  def get_status(self): #whether transfer completed sucsessfully or not
-    return self.sucsess
+          self.save(path= os.path.join(self.output_path, full_name ), image= self.read_image_unicode(file_path))
+          self.num_compressed_image += 1
 
+  def get_status(self): #whether transfer completed sucsessfully or not
+    if (self.num_compressed_image > 0 ):
+      return True
+    else:
+      return False
+  
   def name_without_ext(self, file_name):
     """seperates name and extension
 
--- MainWindow.py
+++ MainWindow.py
@@ -13,15 +13,18 @@ class MainWindow():
         self.output_path = None
         self.jpeg_quality = 10
         self.check_for_completion = None
+
         self.progress_label_text = StringVar()
-        master.title("Compress.me")
         self.ImageSizeReducerObject = ImageSizeReducer
         self.master = master
-        self.master.geometry("400x300")
+        self.master.title("Compress.me")
+        self.master.geometry("500x400")
 
         self.create_jpeg_field()
         self.create_input_button()
-        # self.create_separator()
+
+        self.create_width_height_field()
+
         self.create_confirmation_button()
         self.create_progress_label()
         self.label_about()
@@ -31,27 +34,30 @@ class MainWindow():
 
     def create_progress_label(self):
         self.progress_label = Label(self.master, textvariable= self.progress_label_text)
-        self.progress_label.pack(padx=10,pady=10)
+        self.progress_label.pack(padx=10)
 
     def set_progress_label_text(self):
         if (self.check_for_completion is None):
             self.progress_label_text.set("")
         elif(self.check_for_completion == True):
-            self.progress_label_text.set("Transfer Completed \n Selected folder {}".format(self.input_path)) 
+            self.progress_label_text.set("Compress Completed \n Number of Converted Images: {} \n Selected folder {}"
+                                        .format(self.ImageSizeReducerObject.get_num_compressed_image(), self.input_path))
         else:
-            self.progress_label_text.set("Transfer Failed, please try again... \n Selected Folder {}".format(self.input_path))
+            self.progress_label_text.set("Compress Failed, Check your input Folder... \n Number of Converted Images: {} \n Selected Folder {}"
+                                        .format(self.ImageSizeReducerObject.get_num_compressed_image(), self.input_path))
 
 
     def run(self):
         if (self.input_path and self.jpeg_quality.get() <= 100 and self.jpeg_quality.get() > 0):
             self.ImageSizeReducerObject.set_input_path(self.input_path)
             self.ImageSizeReducerObject.set_jpg_quality(self.jpeg_quality.get())
+            self.ImageSizeReducerObject.set_dim( width= self.width.get(), height=self.height.get()  )
             self.check_for_completion = self.ImageSizeReducerObject.run()
             self.set_progress_label_text()
 
     def create_confirmation_button(self):
         self.confirmation_button = Button(self.master, text="Start converting", command=self.run )
-        self.confirmation_button.pack(padx=10,pady=10)
+        self.confirmation_button.pack(padx=10)
 
     def create_input_button(self):
         self.input_button = Button(self.master, text = "Select Images Folder",command = self.file_dialog_input_path)
@@ -62,11 +68,22 @@ class MainWindow():
 
     def create_jpeg_field(self):
         self.preview_label = Label(self.master, text="Image Quality (1 - 99) higher number produces better image quality" )
-        self.jpeg_quality = IntVar()
+        self.jpeg_quality = IntVar(value=20)
         self.jpeg_text = Entry(self.master, textvariable = self.jpeg_quality, bd = 5)
         self.preview_label.pack()
         self.jpeg_text.pack(padx=10,pady=10)
 
+    def create_width_height_field(self):
+        self.width_height_label = Label(self.master, text="Output image size (Width then height)" )
+        self.width = IntVar(value=800)
+        self.height = IntVar(value=500)
+
+        self.width_text = Entry(self.master, textvariable = self.width, bd = 5)
+        self.height_text = Entry(self.master, textvariable = self.height, bd = 5)
+
+        self.width_height_label.pack()
+        self.height_text.pack(padx=10)
+        self.width_text.pack()
 
     def file_dialog_input_path(self):
         self.input_path = filedialog.askdirectory(initialdir="//",title='Please select a directory')
